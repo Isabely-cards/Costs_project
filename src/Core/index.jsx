9eb493ad0cbf212e27b282f5d415a5dc6841d35b
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-09-04 09:53:39 -0300

    Update index.jsx

--- src/Core/index.jsx
+++ src/Core/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/Core/NavBar"
-import Footer from "../components/Core/Foooter"
+import Footer from "../components/Core/Footer"
 import Container from "../components/Container"
 
 function Core() {
